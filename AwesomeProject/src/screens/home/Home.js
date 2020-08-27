d5c42467d59ec7b5b037b4270996b4d8372ebd52
chamod_lab <chamodlakmal97@gmail.com>
2020-08-27 20:04:46 +0530

    article page created

--- AwesomeProject/src/screens/home/Home.js
+++ AwesomeProject/src/screens/home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flex: 1,
+        backgroundColor: 'yellow',
+        justifyContent:'center',
+        alignItems:'center',
+    }
+});
 
 export default function Home() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Home</Text>
         </View>
     )
