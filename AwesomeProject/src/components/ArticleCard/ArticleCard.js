b1de6848f9887bb78cf7883f26693e0479666e12
chamod_lab <chamodlakmal97@gmail.com>
2020-08-27 22:44:36 +0530

    news page creaing

--- AwesomeProject/src/components/ArticleCard/ArticleCard.js
+++ AwesomeProject/src/components/ArticleCard/ArticleCard.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native';
+import COLORS from '../../utils/constants';
+
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%',
+        width: '95%',
         height: 200,
-        backgroundColor: 'yellow'
     },
     image: {
         width: '100%',
         borderRadius: 5,
         aspectRatio: 16 / 9,
+        resizeMode: "contain",
+    },
+    title: {
+        color: 'black',
+        fontSize: 14,
+        fontWeight: 'bold',
+
+    },
+    bottomTextContainer: {
+        flexDirection: 'row',
     }
 })
 
@@ -18,7 +29,11 @@ export default function ArticleCard() {
     return (
         <View style={styles.container}>
             <Image source={{ uri: 'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg' }} style={styles.image} />
-            <Text>Article</Text>
+            <Text style={styles.title}>Article</Text>
+            <View style={styles.bottomTextContainer}>
+                <Text >New York Time </Text>
+                <Text >4 hours ago</Text>
+            </View>
         </View>
     )
 }
