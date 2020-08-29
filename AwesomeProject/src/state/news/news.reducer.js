057c45040849e7779d222c30b30a61f65c7d54eb
chamod_lab <chamodlakmal97@gmail.com>
2020-08-29 19:22:21 +0530

    updated

--- AwesomeProject/src/state/news/news.reducer.js
+++ AwesomeProject/src/state/news/news.reducer.js
@@ -1,5 +1,18 @@
+import { SUCCESS } from '../../utils/constants';
 import newsActionsTypes from './news.types';
 
 const initialState = {
-    articles: []
-}
+    articles: [],
+};
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        case newsActionsTypes.GET_ARTICLES + SUCCESS:
+            return {
+                ...state,
+                articles: payload
+            };
+        default:
+            return state;
+    }
+};
---
+++ AwesomeProject/src/state/root.saga.js
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects';
+import requestSaga from './request.saga';
+import newsSaga from './news/news.saga';
+import userSaga from './user/user.saga';
+
+export default function* rootSaga() {
+    yield all([requestSaga(), newsSaga(), userSaga()]);
+}
